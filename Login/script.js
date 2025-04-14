7951dedf99cc50da4a7fac129a704bebc987bace
vovchik228378 <vovaloparev2000@gmail.com>
2025-04-14 22:36:42 +0300

    проверка путей на index.html

--- Login/script.js
+++ Login/script.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('/Login/login.html', {
+        const response = await fetch('http://localhost:5000/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
